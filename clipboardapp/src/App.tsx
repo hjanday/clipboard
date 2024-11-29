5035c45de091c1e54ec0edb1aca496666862d8d0
hjanday <jandaypreet@gmail.com>
2024-11-28 23:04:26 -0500

    add input adding

--- clipboardapp/src/App.tsx
+++ clipboardapp/src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ChangeEvent, SetStateAction, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // this is state - use as mutable object (let it be empty object)
+  const [data, setData] = useState({
+    userData: ""
+  });
+  
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setData({
+      ...data, 
+      userData: event.target.value,
+    });
+  };
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+
+      <h1>Clipboard</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        {/* Define user submission for their data */}
+        <input name="user-submit" value={data.userData} onChange={handleInputChange}/>
+
+        {/* Create form */}
+        <button onClick={() => console.log(data.userData)}>
+          Send Input
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
---
+++ clipboardapp/todo.md
@@ -0,0 +1,6 @@
+# Clipboard App Todo
+
+- Create button to create and render a new entry
+- Enable text validation
+- Set 100 char limit
+
